7414e0060126cc2b58ca2042554cf9e078309a60
Bálint Heibl <Balint.Heibl@hu.ibm.com>
2020-12-02 19:00:05 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
 with open("1.txt", "r") as f:
     af = f.readlines()
-    [[print(int(y) * int(x)) for y in af if int(x) + int(y) == 2020] for x in af]
-    [[[print(int(y) * int(x) * int(z)) for z in af if int(x) + int(y) + int(z) == 2020] for y in af] for x in af]
+    [print(int(x) * int(y)) for x in af for y in af if int(x) + int(y) == 2020]
+    [print(int(x) * int(y) * int(z)) for x in af for y in af for z in af if int(x) + int(y) + int(z) == 2020]
