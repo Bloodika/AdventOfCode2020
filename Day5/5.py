6538d133de06783790d8cdafd4b0a6bd33ca0f07
Bálint Heibl <Balint.Heibl@hu.ibm.com>
2020-12-06 01:56:53 +0100

    5th day 2nd part done

--- Day5/5.py
+++ Day5/5.py
@@ -10,21 +10,29 @@ def get_taken_seats(codes):
             col_half = (min_col + max_col) / 2
             max_row = math.floor(row_half) if c == "F" else max_row
             min_row = math.ceil(row_half) if c == "B" else min_row
-            last_row = min_row if c == "F" else last_row
+            last_row = min_row if c == "F" else max_row
 
             max_col = math.floor(col_half) if c == "L" else max_col
             min_col = math.ceil(col_half) if c == "R" else min_col
             last_col = min_col if c == "L" else max_col
+
         counted = (last_row * 8) + last_col
         max_f.append(counted)
+
     return max_f
+
+
+def get_my_seat(taken_seats):
+    for a in range(6, max(taken_seats)):
+        if a not in taken_seats:
+            return a
 
 
 def main():
     with open("5.txt", "r") as f:
-        codes = [line.strip() for line in f.readlines()]
+        codes = f.readlines()
         taken_seats = get_taken_seats(codes)
-        print(max(taken_seats))
+        print(get_my_seat(taken_seats))
 
 
 main()
