f29611923bdf26b29866a29f41b19eb53103d4da
Bálint Heibl <Balint.Heibl@hu.ibm.com>
2020-12-13 14:15:41 +0100

    8th day done

---
+++ Day8/8.py
@@ -0,0 +1,56 @@
+import copy
+
+
+def search_for_loop(lines):
+    steps = []
+    index = 0
+    acc = 0
+    while index not in steps:
+        steps.append(index)
+        try:
+            if "acc" in lines[index]:
+                acc += int(lines[index].split("acc")[1])
+                index += 1
+            elif "nop" in lines[index]:
+                index += 1
+            elif "jmp" in lines[index]:
+                index += int(lines[index].split("jmp")[1])
+        except:
+            print("haha",acc)
+
+    return {"acc": acc, "in_list": (index not in steps)}
+
+
+def search_in_list(s, li):
+    indexes = []
+    for l in range(0, len(li)):
+        if s in li[l]:
+            indexes.append(l)
+    return indexes
+
+
+def fix_loop(lines):
+    steps = []
+    index = 0
+    acc = 0
+    found = False
+    nops = search_in_list("nop", lines)
+    jmps = search_in_list("jmp", lines)
+    for i in nops:
+        copy_lines = copy.deepcopy(lines)
+        copy_lines[i] = copy_lines[i].replace("nop", "jmp")
+        print(search_for_loop(copy_lines))
+    for j in jmps:
+        copy_lines = copy.deepcopy(lines)
+        copy_lines[j] = copy_lines[j].replace("jmp", "nop")
+        print(search_for_loop(copy_lines))
+
+
+def main():
+    with open("8.txt", "r") as f:
+        lines = f.readlines()
+        search_for_loop(lines)
+        fix_loop(lines)
+
+
+main()
